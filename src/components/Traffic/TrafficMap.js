a0c646a2097283fb406c56545168cd286da9b658
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-06-17 20:14:44 +0200

    first map

--- src/components/Traffic/TrafficMap.js
+++ src/components/Traffic/TrafficMap.js
@@ -1,13 +1,38 @@
-import React from "react";
-import { Text, View} from 'react-native';
+import React, { useState } from "react";
+import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import axios from 'axios';
 
 
-const TrafficMap = () =>{
-    return(
+const TrafficMap = ({route}) => {
+    const {props} = route.params;
+
+    const [coords,setCoords] = useState([]);
+    //use Polyline and Callout from react-native-maps
+    //https://github.com/react-native-maps/react-native-maps
+    //chercher par lines/line: id recup dans l'autre page
+    
+    return (
         <View>
-            <Text>TrafficMap</Text>
+            <MapView
+                style={styles.map}
+                provider={MapView.PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: 48.8534,
+                    longitude: 2.3488,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.04
+                }}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+})
 
 export default TrafficMap;
