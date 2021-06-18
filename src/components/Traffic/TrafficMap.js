4de50e289ba1993c97efea826698b7d462ca2999
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-06-18 13:57:05 +0200

    add marker

--- src/components/Traffic/TrafficMap.js
+++ src/components/Traffic/TrafficMap.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import axios from 'axios';
 
 
@@ -8,10 +8,60 @@ const TrafficMap = ({route}) => {
     const {props} = route.params;
 
     const [coords,setCoords] = useState([]);
+
+    const formatLines = (item) => {
+        return {
+            coord: item.coord,
+            id: item.id,
+        }
+    }
+
+    const fetchData = async () => {
+        try {
+            const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode%3AMetro/lines/"+props.id+"/stop_areas?", {
+                headers: {
+                    'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
+                }
+            })
+            return resp
+
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
+
+    const showResults = () => {
+        console.log("showing")
+        const data = fetchData();
+        Promise.resolve(data).then((response) => {
+            const line = new Array;
+            const stop_areas = response.data.stop_areas;
+            
+            for (var i = 0; i < stop_areas.length; i++) {
+                line.push(formatLines(stop_areas[i]));
+                console.log(line)
+            }
+            
+            line.forEach((d) =>{
+                coord = {coord: d.coord}
+                setCoords([...coords,coord]);
+            })
+        })
+    }
+   
+    console.log(coords);
     //use Polyline and Callout from react-native-maps
     //https://github.com/react-native-maps/react-native-maps
     //chercher par lines/line: id recup dans l'autre page
     
+    useEffect(() => {
+        const timeout = setTimeout(showResults, 1000);
+        return () => {
+            clearTimeout(timeout);
+            //setListLines([]);
+        };
+    }, [coords]);
+
     return (
         <View>
             <MapView
@@ -24,6 +74,14 @@ const TrafficMap = ({route}) => {
                     longitudeDelta: 0.04
                 }}
             />
+            {
+                coords.map(({coord}) => 
+                    <Marker
+                        pinColor='blue'
+                        coordinate={{latitude:coord.lat,longitude:coord.long}}
+                    />
+                )
+            }
         </View>
     )
 }
