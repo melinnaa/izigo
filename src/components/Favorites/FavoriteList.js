50b5c01f1f6f44fff2b38f12a114fbab06361048
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-06-18 09:33:11 +0200

    some clean

--- src/components/Favorites/FavoriteList.js
+++ src/components/Favorites/FavoriteList.js
@@ -4,48 +4,26 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import * as firebase from "firebase";
 
 const FavoriteList = ({ navigation }) => {
-    const [data, setData] = useState([
-        { id: 1, course: "Pont de Sèvres - Jussieu", tempsMoyen: "50", horaires: "17h-19h", correspondances: [{ transport: "metro", ligne: "9", depart: "Pont de Sèvres", arrivee: "Michel-Ange Moltor" }, { tranport: "metro", ligne: "10", depart: "Michel-Ange Moltor", arrivee: "Jussieu" }], affluence: "moyenne", utilisation: "5" },
-        { id: 2, course: "Chatillon-Montrouge - Balard", tempsMoyen: "23", horaires: "8h-10h", correspondances: [{ transport: "metro", ligne: "13", depart: "Chatillon-Montrouge", arrivee: "Portes de Vanves" }, { transport: "tramway", ligne: "3a", depart: "Portes de Vanves", arrivee: "Balard" }], affluence: "elevée", utilisation: "3" },
-        { id: 3, course: "Mairie d'Issy - Bir Hakeim", tempsMoyen: "34", horaires: "14h-16h", correspondances: [{ transport: "metro", ligne: "12", depart: "Mairie d'Issy", arrivee: "Pasteur" }, { transport: "metro", ligne: "6", depart: "Pasteur", arrivee: "Bir-Hakeim" }], affluence: "Faible", utilisation: "1" },
-        { id: 4, course: "Pigalle - Stade Charlety", tempsMoyen: "34", horaires: "20h-22h", correspondances: [{ transport: "metro", ligne: "2", depart: "Pigalle", arrivee: "La Chapelle" }, { transport: "rer", ligne: "B", depart: "Gare du Nord", arrivee: "Cité-Universitaire" }], affluence: "moyenne", utilisation: "2" }
-    ]);
 
     const [myData, setMyData] = useState([])
 
     const db = firebase.firestore();
 
-    /*db.collection("Course")
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            id = doc.id;
-            data = [];
-            data.push(doc.data());
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });*/
+    db.collection("Course")
+        .get()
+        .then((querySnapshot) => {
+            let d = querySnapshot.docs.map((doc) => {
+                const id = doc.id;
+                const donnees = doc.data();
 
-    //const getData = () => {
-        db.collection("Course")
-            .get()
-            .then((querySnapshot) => {
-                let d = querySnapshot.docs.map((doc) => {
-                    const id = doc.id;
-                    const donnees = doc.data();
-                   
-                    return {id, ...donnees };
-                });
-                setMyData(d);
-            })
-            .catch((error) => {
-                console.log("Error getting documents: ", error);
+                return { id, ...donnees };
             });
-    //}
+            setMyData(d);
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
 
     return (
         <View style={styles.container}>
