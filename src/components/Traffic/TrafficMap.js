1b15826cd6bd1c073a0bbec7fa86c05444846df2
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-06-20 20:48:15 +0200

    add disruptions

--- src/components/Traffic/TrafficMap.js
+++ src/components/Traffic/TrafficMap.js
@@ -10,16 +10,17 @@ const TrafficMap = ({ route }) => {
 
     const [coords, setCoords] = useState([]);
     const [linePoints, setLinePoints] = useState([]);
+    const [lineReports, setLineReports] = useState([]);
 
     const formatLines = (item) => {
         return {
             /*coord: item.coord,
             id: item.id,
             label: item.label*/
-            sections:item.sections,
-            departure_date_time:item.departure_date_time,
-            arrival_date_time:item.arrival_date_time,
-            stop_date_times:item.sections[1].stop_date_times
+            sections: item.sections,
+            departure_date_time: item.departure_date_time,
+            arrival_date_time: item.arrival_date_time,
+            stop_date_times: item.sections[1].stop_date_times
         }
     }
 
@@ -27,7 +28,7 @@ const TrafficMap = ({ route }) => {
 
     const fetchData = async () => {
         try {
-            const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/journeys?from="+props.fromLon+";"+props.fromLat+"&to="+props.toLon+";"+props.toLat+"&", {
+            const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/journeys?from=" + props.fromLon + ";" + props.fromLat + "&to=" + props.toLon + ";" + props.toLat + "&", {
                 headers: {
                     'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
                 }
@@ -39,32 +40,19 @@ const TrafficMap = ({ route }) => {
         }
     }
 
-    /*const fetchLineReports = async() =>{
-        console.log("showing")
-        const data = fetchData();
-        Promise.resolve(data).then((response) => {
-            const line = new Array;
-            const stop_areas = response.data.stop_areas;
-
-            for (var i = 0; i < stop_areas.length; i++) {
-                line.push(formatLines(stop_areas[i]));
-            }
-
-            line.forEach((d) => {
-                try {
-                    const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/stop_areas/"+d.id+"/physical_modes/physical_mode%3AMetro/line_reports?", {
-                        headers: {
-                            'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
-                        }
-                    })
-                    return resp
-        
-                } catch (err) {
-                    console.log(err.response);
+    const fetchLineReports = async () => {
+        try {
+            const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/lines/" + props.id + "/line_reports?", {
+                headers: {
+                    'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
                 }
             })
-        })
-    }*/
+            return resp
+
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
 
     const showResults = () => {
         console.log("showing")
@@ -72,70 +60,84 @@ const TrafficMap = ({ route }) => {
         Promise.resolve(data).then((response) => {
             const line = new Array;
             const journeys = response.data.journeys;
-            
+
             for (var i = 0; i < journeys.length; i++) {
                 line.push(formatLines(journeys[i]));
             }
-            
+
             var coordinates = [];
             line.forEach((d) => {
-                for(i=0;i<d.stop_date_times.length;i++){
-                    var coord = {latitude: parseFloat(d.stop_date_times[i].stop_point.coord.lat), longitude:parseFloat(d.stop_date_times[i].stop_point.coord.lon), name:d.stop_date_times[i].stop_point.name}
+                for (i = 0; i < d.stop_date_times.length; i++) {
+                    var coord = { latitude: parseFloat(d.stop_date_times[i].stop_point.coord.lat), longitude: parseFloat(d.stop_date_times[i].stop_point.coord.lon), name: d.stop_date_times[i].stop_point.name, departure_time: d.stop_date_times[i].departure_date_time, arrival_time: d.stop_date_times[i].arrival_date_time }
                     coordinates = [...coordinates, coord];
                 }
-                //var coord = { latitude: parseFloat(d.coord.lat), longitude: parseFloat(d.coord.lon), label:d.label }
-                //coordinates = [...coordinates, coord];
-                //console.log(coordinates);
-                //setCoords([...coords,coord]);
             })
             setCoords(coordinates);
         })
 
     }
 
-    /*const showReports = () =>{
+    //Voir comment transformer dates pour avoir temps d'arrivee
+
+    const showReports = () => {
         console.log("showing")
+        const journey = fetchData();
         const data = fetchLineReports();
-        Promise.resolve(data).then((response) => {
-            const line = new Array;
-            const line_reports = response.data.line_reports;
+        Promise.resolve(journey).then((response) => {
+            Promise.resolve(data).then((response2) => {
+                const line = new Array;
+                const report = new Array;
+                const line_stop_areas = response.data.journeys;
+                const line_reports = response2.data.line_reports;
+                
+                for (var i = 0; i < line_stop_areas.length; i++) {
+                    line.push(formatLines(line_stop_areas[i]));
+                }
 
-            for (var i = 0; i < line_reports.length; i++) {
-                line.push(formatLines(line_reports[i]));
-            }
+                for (var i = 0; i < line_reports.length; i++) {
+                    report.push(formatLines(line_reports[i]));
+                }
 
-            var coordinates = [];
-            line.forEach((d) => {
-                var coord = { latitude: parseFloat(d.coord.lat), longitude: parseFloat(d.coord.lon), label:d.label }
-                coordinates = [...coordinates, coord];
-                console.log(coordinates);
-                //setCoords([...coords,coord]);
+                console.log(line);
+                line.forEach((d) => {
+                    report.forEach((rep) => {
+                        for (i = 0; i < d.stop_date_times.length; i++) {
+                            if(d.stop_date_times[i].stop_point.name== rep.pt_objects.name){
+                                var disruption = '1 perturbation(s)';
+                                setLineReports([...lineReports,disruption])
+                            }
+                            else{
+                                disruption = '0 perturbation(s)';
+                                setLineReports([...lineReports,disruption])
+                            }
+                        }
+                    })
+                    
+                })
+    
             })
-            setCoords(coordinates);
+            
         })
-    }*/
+    }
 
-    const createPolyline = () =>{
+    const createPolyline = () => {
         let latLong = [];
-        coords.map(({latitude,longitude}) => {
-            latLong.push({latitude:latitude,longitude:longitude});
+        coords.map(({ latitude, longitude }) => {
+            latLong.push({ latitude: latitude, longitude: longitude });
         })
-        
+
         setLinePoints(latLong);
     }
-
-    //use Polyline and Callout from react-native-maps
-    //https://github.com/react-native-maps/react-native-maps
-    //chercher par lines/line: id recup dans l'autre page
 
     useEffect(() => {
         const timeout = setTimeout(showResults, 1000);
         createPolyline();
+        //showReports();
         return () => {
             clearTimeout(timeout);
         };
     }, [coords]);
-    //console.log(coords);
+
     return (
         <View>
             <MapView
@@ -149,29 +151,28 @@ const TrafficMap = ({ route }) => {
                 }}
             >
                 {
-                    coords.map(({ latitude, longitude, name }) =>
+                    coords.map(({ latitude, longitude, name, departure_time, arrival_time }) =>
                         <Marker
                             coordinate={{
                                 latitude: latitude,
                                 longitude: longitude
                             }}
-                            //pinColor='red'
-                            image={require("../../assets/map_marker.png")}
+                            image={require("../../assets/map_marker2.png")}
                         >
                             <Callout tooltip>
                                 <View style={styles.calloutContainer}>
-                                    <View style={{flexDirection:'row', marginBottom:5}}>
+                                    <View style={{ flexDirection: 'row', marginBottom: 5 }}>
                                         <Text style={styles.calloutText}>{props.code} - </Text>
                                         <Text style={styles.calloutText}>{name}</Text>
                                     </View>
-                                    <View style={{flexDirection:'row'}}>
+                                    <View style={{ flexDirection: 'row' }}>
                                         <View style={styles.timeContainer}>
                                             <Ionicons name="subway-outline" size={20} color="#70d8a2" />
-                                            <Text style={styles.timeText}>3 min</Text>
+                                            <Text style={styles.timeText}>D: {departure_time.substr(-6).substr(0, 2) + ":" + departure_time.substr(-6).substr(2, 2)}</Text>
                                         </View>
                                         <View style={styles.timeContainer}>
                                             <Ionicons name="subway-outline" size={20} color="#70d8a2" />
-                                            <Text>8 min</Text>
+                                            <Text style={styles.timeText}>A: {arrival_time.substr(-6).substr(0, 2) + ":" + arrival_time.substr(-6).substr(2, 2)}</Text>
                                         </View>
                                     </View>
                                     <View style={styles.reportsContainer}>
@@ -187,7 +188,7 @@ const TrafficMap = ({ route }) => {
                     strokeColor="#000000" // fallback for when `strokeColors` is not supported by the map-provider
                     strokeWidth={4}
                 />
-                
+
             </MapView>
         </View>
     )
@@ -198,37 +199,37 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
     },
-    calloutContainer:{
-        backgroundColor:'white',
-        borderRadius:4,
-        paddingVertical:10,
-        paddingHorizontal:15,
-        width:250,
-        height:150,
-        alignItems:'center',
-        justifyContent:'center'
+    calloutContainer: {
+        backgroundColor: 'white',
+        borderRadius: 4,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        width: 200,
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    calloutText:{
-        color:'#000000',
-        fontFamily:'NunitoBold',
-        fontSize:13
+    calloutText: {
+        color: '#000000',
+        fontFamily: 'NunitoBold',
+        fontSize: 13
     },
-    timeContainer:{
-        paddingHorizontal:10,
-        paddingVertical:5,
-        flexDirection:'row'
+    timeContainer: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        flexDirection: 'row'
     },
-    timeText:{
-        fontFamily:'NunitoRegular',
-        fontSize:14
+    timeText: {
+        fontFamily: 'NunitoRegular',
+        fontSize: 14
     },
-    reportsContainer:{
-        paddingVertical:5,
-        flexDirection:'row'
+    reportsContainer: {
+        paddingVertical: 5,
+        flexDirection: 'row'
     },
-    reportsText:{
-        fontSize:16,
-        fontFamily:'NunitoBold'
+    reportsText: {
+        fontSize: 16,
+        fontFamily: 'NunitoBold'
     }
 })
 
