be14d7b66e588902367c6333e86053ebcc7ddb30
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-06-21 13:51:35 +0200

    tram and RER for filters

--- src/components/Traffic/TrafficFilters.js
+++ src/components/Traffic/TrafficFilters.js
@@ -4,101 +4,131 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import MapView from 'react-native-maps';
 import axios from 'axios';
 
-const TrafficFilters= ({navigation}) => {
+const TrafficFilters = ({ navigation }) => {
     const [line, setLine] = useState("");
     const [station, setStation] = useState("");
     const [listResults, setListResults] = useState([]);
     const [listLines, setListLines] = useState([]);
+    const [listTram, setListTram] = useState([]);
     const [listStation, setListStation] = useState([]);
-    const [isShowingResults, setIsShowingResults] = useState(false);
 
     const formatLines = (item) => {
         return {
-            fromLon:item.routes[0].direction.stop_area.coord.lon,
-            fromLat:item.routes[0].direction.stop_area.coord.lat,
-            toLon:item.routes[1].direction.stop_area.coord.lon,
-            toLat:item.routes[1].direction.stop_area.coord.lat,
+            fromLon: item.routes[0].direction.stop_area.coord.lon,
+            fromLat: item.routes[0].direction.stop_area.coord.lat,
+            toLon: item.routes[1].direction.stop_area.coord.lon,
+            toLat: item.routes[1].direction.stop_area.coord.lat,
             name: item.name,
             id: item.id,
             code: item.code
         }
     }
 
-    const formatStations = (item) =>{
+    const formatRER = (item) =>{
         return{
-            label:item.label,
             id:item.id,
-            name:item.name
+            code:item.network.name+" "+item.code,
+            name:item.routes[0].name,
+            color:item.color,
+            routes:item.routes
+        }
+    }
+
+    const formatStations = (item) => {
+        return {
+            label: item.label,
+            id: item.id,
+            name: item.name
         }
     }
 
     const showResults = () => {
         console.log("showing")
         const data = fetchData();
-        const dataTram = fetchDataTram();
+
         Promise.resolve(data).then((response) => {
             const transport = new Array;
             const lignes = response.data.lines;
-            
+
             for (var i = 0; i < lignes.length; i++) {
                 transport.push(formatLines(lignes[i]));
-               
             }
 
-            setIsShowingResults(true);
-            setListResults(transport);
-            
-            transport.forEach((d) =>{
-                if(d.code==line){
-                    search = {id: d.id, name:d.name, code:d.code, fromLon:d.fromLon,fromLat:d.fromLat,toLon:d.toLon,toLat:d.toLat}
-                    setListLines([...listLines,search]);
+            transport.forEach((d) => {
+                if (d.code == line) {
+                    search = { id: d.id, name: d.name, code: d.code, fromLon: d.fromLon, fromLat: d.fromLat, toLon: d.toLon, toLat: d.toLat }
+                    setListLines([...listLines, search]);
+                    setListResults([...listResults,search]);
                 }
             })
         })
 
-        /*Promise.resolve(dataTram).then((response) => {
+    }
+    const showResultsTram = () => {
+        console.log("showing");
+        const dataTram = fetchDataTram();
+        Promise.resolve(dataTram).then((response) => {
+            const transport = new Array;
+            const lignes = response.data.lines;
 
-            const tram = new Array;
-            const tramLines = response.data.lines;
-
-            for (var i = 0; i < tramLines.length; i++) {
-                tram.push(formatLines(tramLines[i]));
+            for (var i = 0; i < lignes.length; i++) {
+                transport.push(formatLines(lignes[i]));
             }
-            setIsShowingResults(true);
-        
 
-            tram.forEach((d) =>{
-                if(d.code==line){
-                    search = {id: d.id, name:d.name, code:d.code}
-                    setListLines([...listLines,search]);
+            transport.forEach((d) => {
+                if (d.code==line) {
+                    var search = { id: d.id, name: d.name, code: d.code, fromLon: d.fromLon, fromLat: d.fromLat, toLon: d.toLon, toLat: d.toLat }
+                    //setListTram([...listTram, search]);
+                    setListResults([...listResults,search]);
                 }
-                
             })
-
-        })*/
+        })
     }
 
-    const showResultsStation = () =>{
+    const showResultsRER = () =>{
+        console.log("showing");
+        const dataRER = fetchDataRER();
+        Promise.resolve(dataRER).then((response) => {
+            const transport = new Array;
+            const lignes = response.data.lines;
+
+            for (var i = 0; i < lignes.length; i++) {
+                transport.push(formatRER(lignes[i]));
+            }
+
+            transport.forEach((d) => {
+                if (d.code==line) {
+                    var search = { id: d.id, name: d.name, code: d.code, color:d.color, routes:d.routes}
+                    setListTram([...listTram, search]);
+                    setListResults([...listResults,search]);
+                }
+            })
+        })
+
+    }
+
+    const showResultsStation = () => {
         console.log("showing")
         const data = fetchStopAreas();
-       
+
         Promise.resolve(data).then((response) => {
             //ça passe
             const transport = new Array;
             const lines = response.data.lines;
-            console.log(response.data.lines);
             
+
             for (var i = 0; i < lines.length; i++) {
                 transport.push(formatLines(lines[i]));
             }
-            //console.log(lines);
-            transport.forEach((d) =>{
-                search = {id: d.id, name:d.name, code:d.code, physical_modes:d.physical_modes}
-                setListStation([...listStation,search]);
+            transport.forEach((d) => {
+                console.log(d);
+                var search = { id: d.id, name: d.name, code: d.code, physical_modes: d.physical_modes }
+                setListStation([...listStation, search]);
             })
         })
+        
     }
-   
+
     const fetchData = async () => {
         try {
             const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode%3AMetro/lines?", {
@@ -114,7 +144,7 @@ const TrafficFilters= ({navigation}) => {
     }
 
 
-    const fetchDataTram = async () =>{
+    const fetchDataTram = async () => {
         try {
             const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode%3ATramway/lines?", {
                 headers: {
@@ -128,7 +158,21 @@ const TrafficFilters= ({navigation}) => {
         }
     }
 
-    const fetchDataStation = async() =>{
+    const fetchDataRER = async () =>{
+        try {
+            const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode%3ARapidTransit/lines?", {
+                headers: {
+                    'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
+                }
+            })
+            return resp
+
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
+
+    const fetchDataStation = async () => {
         try {
             const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/stop_areas?", {
                 headers: {
@@ -142,33 +186,30 @@ const TrafficFilters= ({navigation}) => {
         }
     }
 
-    const fetchStopAreas = async () =>{
+    const fetchStopAreas = async () => {
         console.log("showing")
         const data = fetchDataStation();
         Promise.resolve(data).then((response) => {
             const transport = new Array;
             const stop_areas = response.data.stop_areas;
-            
+
             for (var i = 0; i < stop_areas.length; i++) {
                 transport.push(formatStations(stop_areas[i]));
             }
-            
-            transport.forEach(async (d) =>{
-                
-                //if(d.label == station){
-                    try {
-                        const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/stop_areas/"+d.id+"/lines?", {
-                            headers: {
-                                'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
-                            }
-                        })
-                        
-                        return resp
-            
-                    } catch (err) {
-                        console.log(err.response);
-                    }
-                //}
+
+            transport.forEach(async (d) => {
+                try {
+                    const resp = await axios.get("https://api.navitia.io/v1/coverage/fr-idf/stop_areas/" + d.id + "/lines?", {
+                        headers: {
+                            'Authorization': `7a9c06ed-e0b6-4bc3-a7da-f27d4cbee972`,
+                        }
+                    })
+
+                    return resp
+
+                } catch (err) {
+                    console.log(err.response);
+                }
             })
         })
     }
@@ -178,6 +219,9 @@ const TrafficFilters= ({navigation}) => {
      */
     useEffect(() => {
         const timeout = setTimeout(showResults, 1000);
+        showResultsTram();
+        showResultsRER();
+        //showResultsStation();
         //const timeout2 = setTimeout(showResultsStation, 1000);
         return () => {
             clearTimeout(timeout);
@@ -185,9 +229,9 @@ const TrafficFilters= ({navigation}) => {
         };
     }, [line, station]);
 
-    console.log(listStation);
+    console.log(listResults);
 
-    if (line == ""){
+    if (line == "") {
         return (
             <View style={styles.container}>
                 <View style={styles.inputsBoxContainer}>
@@ -264,19 +308,19 @@ const TrafficFilters= ({navigation}) => {
                         </View>
                     </View>
                 </View>
-                <SafeAreaView style={{flex:1}}>
+                <SafeAreaView style={{ flex: 1 }}>
                     <FlatList
-                        data={listLines}
-                        renderItem={({item}) => (
+                        data={listResults}
+                        renderItem={({ item }) => (
                             <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate("TrafficMap", { props: item })}>
                                 <Text style={styles.items}>{item.code}</Text>
-                                <Text style={[styles.items,{paddingLeft:5}]}>{item.name}</Text>
+                                <Text style={[styles.items, { paddingLeft: 5 }]}>{item.name}</Text>
                             </TouchableOpacity>
                         )}
                         keyExtractor={(item) => item.id}
                     />
                 </SafeAreaView>
-                <View style={{margin:20, padding:30}}></View>
+                <View style={{ margin: 20, padding: 30 }}></View>
                 <StatusBar style="auto" />
             </View>
         )
@@ -325,7 +369,7 @@ const styles = StyleSheet.create({
         display: "flex",
         alignItems: "center",
         position: "absolute",
-        fontFamily:"NunitoBold"
+        fontFamily: "NunitoBold"
     },
     icon: {
         position: 'absolute',
@@ -336,18 +380,18 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
     },
-    flatList:{
-        flex:1
+    flatList: {
+        flex: 1
     },
-    itemContainer:{
-        flexDirection:'row',
-        paddingVertical:20,
-        paddingLeft:5
+    itemContainer: {
+        flexDirection: 'row',
+        paddingVertical: 20,
+        paddingLeft: 5
     },
-    items:{
-        color:"black",
-        fontSize:14,
-        fontFamily:'NunitoBold'
+    items: {
+        color: "black",
+        fontSize: 14,
+        fontFamily: 'NunitoBold'
     }
 })
 
