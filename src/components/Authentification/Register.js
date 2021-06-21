c516054c1f9d1b4f5412705648fed2c473776ee7
dalinda <aloui.dalinda28@gmail.com>
2021-06-21 17:14:50 +0200

    Revert "add authentification"
    This reverts commit bf86b81df10e75ef81b9e367618bcbf453ed8cf6.

--- src/components/Authentification/Register.js
+++ src/components/Authentification/Register.js
@@ -1,100 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import firebase from '../../Firebase/firebase'
-import Ionicons from "react-native-vector-icons/Ionicons";
-import * as Facebook from 'expo-facebook';
-import * as Google from 'expo-google-app-auth';
-import * as AppleAuthentication from 'expo-apple-authentication';
 
-const Register = ({ navigation }) => {
+const Register = ({navigation}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const signUp = async () => {
+    const signUp = async()=>{
         try {
-            const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-            console.log(userCredential);
-
-            return navigation.navigate('Login');
-        }
-        catch (err) {
-            switch (err.code) {
-                case 'auth/invalid-email':
-                    return Alert.alert('Saisissez un e-mail valide');
-                    break;
-                case 'auth/email-already-in-use':
-                    return Alert.alert('Un compte IziGo associé à cet e-mail existe déja. Veuillez recommencer.');
-                    break;
-                case 'auth/weak-password':
-                    return Alert.alert('Le mot de passe doit comprendre au moins 6 caractères.');
-                    break;
-            }
-            console.log(err.code);
-            return navigation.navigate('Register');
-        }        
-    }
-
-    async function onFacebookButtonPress() {
-        try {
-            await Facebook.initializeAsync({
-                appId: '155973796466454',
-            });
-            const {
-                type,
-                token,
-            } = await Facebook.logInWithReadPermissionsAsync({
-                permissions: ['public_profile'],
-            });
-            if (type === 'success') {
-                // Get the user's name using Facebook's Graph API
-                const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-                Alert.alert('Connecté!', `Bonjour ${(await response.json()).name}!`);
-
-                return navigation.navigate('FavoritePage');
-            } else {
-                return navigation.navigate('Register');
-            }
-        } catch ({ message }) {
-            alert(`Facebook Login Error: ${message}`);
-        }
-
-    }
-
-    const onGoogleButtonPress = async () => {
-        try {
-            const result = await Google.logInAsync({
-                //androidClientId: YOUR_CLIENT_ID_HERE,
-                iosClientId: '811011509063-chv7m4gvfug5c6vav58d6kktiobg5udg.apps.googleusercontent.com',
-                scopes: ['profile', 'email'],
-            });
-
-            if (result.type === 'success') {
-                return navigation.navigate('FavoritePage');
-
-            } else {
-                return navigation.navigate('Register');
-            }
-        } catch (e) {
-            return { error: true };
-        }
-    };
-
-    const onAppleButtonPress = async () => {
-        try {
-            const credential = await AppleAuthentication.signInAsync({
-                requestedScopes: [
-                    AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                    AppleAuthentication.AppleAuthenticationScope.EMAIL,
-                ],
-            });
-            return navigation.navigate('FavoritePage');
-        } catch (e) {
-            if (e.code === 'ERR_CANCELED') {
-                return navigation.navigate('Register');
-            } else {
-                // handle other errors
-            }
+            firebase.auth().createUserWithEmailAndPassword(email, password);
+            navigation.navigate('Login');
+        }catch(err){
+            setError(err.message);
         }
     }
 
@@ -106,10 +24,9 @@ const Register = ({ navigation }) => {
             />
             <Text style={styles.title}>Inscription</Text>
             <TextInput
-                placeholder={'Adresse e-mail'}
+                placeholder={'Email'}
                 onChangeText={setEmail}
                 style={styles.inputName}
-                keyboardType="email-address"
                 returnKeyType='go'
             />
             <TextInput
@@ -120,62 +37,31 @@ const Register = ({ navigation }) => {
                 returnKeyType='go'
             />
             {
-                error ?
-                    <Text style={{ color: 'red' }}>{error}</Text>
-                    : null
+                error?
+                <Text style={{color : 'red'}}>{error}</Text>
+                : null
             }
             <TouchableOpacity
                 activeOpacity={0.7}
                 style={styles.submit}
                 onPress={() => signUp()}
-            >
+                >
                 <Text style={styles.submitText}>S'inscrire</Text>
             </TouchableOpacity>
             <View
                 style={{
                     borderBottomColor: 'black',
-                    borderBottomWidth: 1,
-                    top: "20%",
-                    width: "50%"
+                    borderBottomWidth: 2,
+                    paddingTop:"2%"
                 }}
             ></View>
-            <View>
-                <Text style={styles.text}>Se connecter avec</Text>
-            </View>
-
-            <View style={styles.containerLogo}>
-                <TouchableOpacity
-                    style={styles.icon}
-                    onPress={() => onFacebookButtonPress()}>
-                    <Ionicons name={"logo-facebook"} size={40} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.icon}
-                    onPress={() => onGoogleButtonPress()}>
-                    <Ionicons name={"logo-google"} size={40} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.icon}>
-                    <Ionicons name={"logo-twitter"} size={40} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.icon}>
-                    <Ionicons name={"logo-instagram"} size={40} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.icon}
-                    onPress={() => onAppleButtonPress()}>
-                    <Ionicons name={"logo-apple"} size={40} />
-                </TouchableOpacity>
-            </View>
-
+            <Text style={styles.submitTextRegister}>Avez vous un compte ?</Text>
             <TouchableOpacity
                 activeOpacity={0.7}
                 style={styles.submitSignUp}
                 onPress={() => navigation.navigate('Login')}
-            >
-                <Text style={styles.submitTextLogin}>Avez vous un compte ?</Text>
-                <Text style={styles.submitTextLogin}> Se connecter </Text>
+                >
+                <Text style={styles.submitTextRegister}> Se connecter </Text>
             </TouchableOpacity>
         </View>
     )
@@ -210,7 +96,7 @@ const styles = StyleSheet.create({
         paddingVertical: 0,
         borderColor: '#D3D3D3',
         opacity: 1,
-        top: "15%",
+        top: "10%",
         height: "7%",
         width: "90%",
         marginTop: "2%",
@@ -224,7 +110,7 @@ const styles = StyleSheet.create({
         paddingVertical: 0,
         borderColor: '#D3D3D3',
         opacity: 1,
-        top: "15%",
+        top: "10%",
         height: "7%",
         width: "90%",
         marginTop: "2%",
@@ -234,39 +120,24 @@ const styles = StyleSheet.create({
         paddingLeft: 8,
     },
     submit: {
-        top: "17%",
+        top: "12%",
         width: "55%",
         marginTop: "2%",
         padding: "3%",
         backgroundColor: '#FE596F',
         borderRadius: 40,
         borderWidth: 1,
-        borderColor: '#fff',
-        zIndex: 5
+        borderColor: '#fff'
     },
     submitText: {
         color: '#fff',
         textAlign: 'center',
         fontSize: 25
     },
-    submitTextLogin: {
+    submitTextRegister: {
         color: 'black',
-        top: "580%",
-        color: "#A0A0A0",
-        textAlign: "center"
-    },
-    text: {
-        top: "1100%",
-    },
-    containerLogo: {
-        width: "100%",
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        paddingLeft: "8%",
-        top: "45%"
-    },
-    icon: {
-        padding: "4%",
+        top: 150,
+        color: "#A0A0A0"
     }
 })
 
